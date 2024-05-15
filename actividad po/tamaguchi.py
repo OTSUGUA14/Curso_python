e2dfb694b7997b8d4b30ddbd4b7634b13446c25b
Augusto <otsuguariquelme@gmail.com>
2024-05-15 20:07:12 -0300

    tama listo

--- actividad po/tamaguchi.py
+++ actividad po/tamaguchi.py
@@ -1,13 +1,14 @@
+import time 
 class Tamagotchi:
     nivel_energia=100
-    nivel_hambre=0
+    nivel_hambre=100
     nivel_felicidad=50
     humor="indiferente"
     esta_vivo=True
     def __init__(self,nombre):
         self.nombre=nombre
        
-    def modificador(self,nombre,cantidad,hacer,humor):
+    def modificador(self,nombre,cantidad,hacer):
         if hacer=="resta":
             for i in range(cantidad):
                 if nombre==0:
@@ -21,7 +22,7 @@ class Tamagotchi:
                 else:
                     nombre+=1
         return nombre
-    def modi_humor(self,felicidad):
+    def modi_humor(self,felicidad,humor):
         if felicidad<20:
             humor="enojado"
         elif felicidad>20 and felicidad<40:
@@ -49,13 +50,13 @@ class Tamagotchi:
             self.nivel_felicidad=self.modificador(self.nivel_felicidad,30,"resta")
             self.humor=self.modi_humor(self.nivel_felicidad,self.humor)
             self.nivel_energia=self.modificador(self.nivel_energia,20,"resta")
-            self.nivel_hambre=self.modificador(self.nivel_hambre,10,"suma")
-            pass
+            self.nivel_hambre=self.modificador(self.nivel_hambre,10,"resta")
+            
         else:
             self.nivel_felicidad=self.modificador(self.nivel_felicidad,20,"suma")
             self.humor=self.modi_humor(self.nivel_felicidad,self.humor)
             self.nivel_energia=self.modificador(self.nivel_energia,18,"resta")
-            self.nivel_hambre=self.modificador(self.nivel_hambre,10,"suma")
+            self.nivel_hambre=self.modificador(self.nivel_hambre,10,"resta")
         self.verificar_estado()
     def dormir(self):
         if self.nivel_hambre<=20:
@@ -69,20 +70,59 @@ class Tamagotchi:
             self.nivel_hambre=self.modificador(self.nivel_hambre,5,"suma")
         self.verificar_estado()
     def verificar_estado(self):
-        if self.esta_vivo:
-            print("----------------------")
-            print("El tamagochi esta vivo")
-        else:
-            print("----------------------")
-            print("El tamagochi esta muerto")
-            print("Fin del juego")
+        if self.nivel_energia<=0:
+            self.esta_vivo=False
+      
             
 
 
 
 
-
-
+nombre=input("Para empezar el juego primero ingresa el nombre del tamaguchi: ")
+tama=Tamagotchi(nombre)
+while True:
+    if not tama.esta_vivo:
+        print(nombre, "a muerto :(")
+        break
+    print(""" 
+----------------------------          
+OPCIONES:
+1-Mostrar estado del tamagochi
+2-Alimentar al tamagochi
+3-Jugar con el tamagochi
+4-Dormir al tamagochi
+5- Terminar el juego
+----------------------------    
+          """)
+    opcion=int(input("Elegi la opcion que quieras realizar:"))
+    if opcion==1:
+        print("------ESTADO------")
+        tama.mostrar_estado()
+        print("------------------")
+    if opcion==2:
+        print("------ALIMENTAR------")
+        for i in range(3):
+            print("Alimentando.....")
+            time.sleep(1)
+        tama.alimentar()
+        print("------------------")
+    if opcion==3:
+        print("------JUGAR------")
+        for i in range(3):
+            print("Jugando.....")
+            time.sleep(1)
+        tama.jugar()
+        print("------------------")
+    if opcion==4:
+        print("------DORMIR------")
+        for i in range(3):
+            print("DURMIENDO.....")
+            time.sleep(1)
+        tama.dormir
+        print("------------------")
+    if opcion==5:
+        print("Gracias por jugar ")
+        break
 
 
 
